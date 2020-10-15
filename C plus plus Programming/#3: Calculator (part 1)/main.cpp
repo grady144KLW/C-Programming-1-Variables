a684bab51ea6e67e9500a1ec1a79beb9d1954d6c
grady144KLW <72415409+grady144KLW@users.noreply.github.com>
2020-10-15 20:05:06 +0100

    Add files via upload

---
+++ C plus plus Programming/#3: Calculator (part 1)/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int one;
+   int two;
+
+   cout << "first number ";
+   cin >> one; // asking the user to enter the first number
+
+   cout << "second number ";
+   cin >> two; // asking the user to enter the second number
+
+   cout << one + two; // adds the two numbers that the user has declared
+
+
+    return 0;
+}

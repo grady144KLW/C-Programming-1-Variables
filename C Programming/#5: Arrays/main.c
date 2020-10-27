41a89592d57872810b21fa46c659541210b89f36
grady144KLW <72415409+grady144KLW@users.noreply.github.com>
2020-10-27 05:25:23 -0700

    Add files via upload

---
+++ C Programming/#5: Arrays/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int numbers1[] = {1, 2, 3, 4, 5, 6}; // declaring an array of numbers
+int numbers2[10];// the number tells how many elements the variable is holding
+numbers2[1] = 323; // declaring the second element of the array
+
+printf("%d\n", numbers1[0]); // printing the first index of the array
+printf("%d", numbers2[1]); // printing the second element of the array
+
+    return 0;
+}

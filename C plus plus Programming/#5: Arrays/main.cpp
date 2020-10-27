f98870f34b2d140a2a4af9459cf0873c767b3e57
grady144KLW <72415409+grady144KLW@users.noreply.github.com>
2020-10-27 05:57:35 -0700

    Add files via upload

---
+++ C plus plus Programming/#5: Arrays/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int numbers1[] = {1, 2, 3, 4}; // declaring array of numbers
+  int numbers2[4]; // declaring how many elements the variable can hold
+  numbers2[0] = 1; // assigning the first element to one
+
+cout <<"first integer is: "<< numbers1[0] << endl; // printing the first element of the first variable
+cout <<"second integer is: "<< numbers2[0]; // printing the first element of the second variable
+    return 0;
+}
